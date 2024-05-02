43ea0bd3b076818aedc0b537259655bbe9f4b6b2
Subhath H.R.H <hasirasubath@gmail.com>
2024-05-02 09:22:29 +0530

    fix: btn add

--- resources/views/beneficiary/beneficiary_index.blade.php
+++ resources/views/beneficiary/beneficiary_index.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
 </head>
 <body>
     Beneficiary Detailsssssssss
@@ -46,9 +48,18 @@
                 <td>
                     <a href="{{ route('family/create', ['beneficiaryId' => $beneficiary->id]) }}" class="btn btn-primary">Add Family Member</a>
                 </td>
+                <td>
+                    {{-- <a href="{{ route('beneficiary/edit', ['id' => $beneficiary->id]) }}" class="btn btn-primary">Edit</a>
+                    <a href="{{ route('beneficiary/delete', ['id' => $beneficiary->id]) }}" class="btn btn-danger">Delete</a> --}}
+                    <a href="" class="btn btn-primary">Edit</a>
+                </td>
+                <td>
+                    <a href="" class="btn btn-danger">Delete</a>
+                </td>
             </tr>
             @endforeach
         </tbody>
     </table>
+    {{-- <a href="{{ route('beneficiary/create') }}" class="btn btn-primary">Add Beneficiary</a> --}}
 </body>
 </html>
